cfc39086b820bb463ca1244b9eb7cf72d13eadf7
Andrew LeRoux <andrewleroux2639@gmail.com>
2022-04-12 11:19:07 -0700

    commit tree 05c6b45039791c3b17b955aca49b4b692e028a8f

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,32 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each.with_index do |dwarf, index|
+    puts "#{index + 1}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map do |planeteer|
+    "#{planeteer.capitalize()}!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  bol = false
+  array.each do |element|
+    if element.length > 4
+      bol = true
+    end
+  end
+  bol
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  true_cheese = nil
+  array.each do |cheese|
+    if cheese_types.include?(cheese)
+      true_cheese = cheese
+    end
+  end
+  true_cheese
 end
